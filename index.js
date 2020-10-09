087517d8ffb650ff120f1e9c910631ee3733dd41
dimasmir03 <62016926+dimasmir03@users.noreply.github.com>
2020-10-09 19:07:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
-server.listen(3000, "192.168.88.128");
+server.listen(3000, "");
 
 app.get("/", function (request, respons) {
    respons.sendFile(__dirname + "/index.html");
